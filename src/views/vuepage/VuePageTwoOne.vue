9f4346bc2976d0402762946a68b5a52c3a7eca36
WayneLu <wayne23123@gmail.com>
2023-09-08 14:19:10 +0800

    vue2-1頁面更新

--- src/views/vuepage/VuePageTwoOne.vue
+++ src/views/vuepage/VuePageTwoOne.vue
@@ -320,6 +320,34 @@ function locate(idPound) {
             </div>
             <div><span class="brackets">})</span></div>
             <br />
+            <p class="fz32 cor36">v-bind 對樣式控制的增強 - 操作 class</p>
+            <br />
+            <p class="fz28">語法 :class="物件/數組"</p>
+            <br />
+            <div class="fz30">
+              <span class="then">＜</span> <span class="number">div </span>
+              <span class="function">class </span> <span class="then">=</span>
+              <span class="src">"box"</span>
+              <span class="function">:class</span>
+              <span class="then">=</span>
+              <span class="src">"{類名1:布林值,類名2:布林值}"</span>
+              <span class="then">>＜/ </span> <span class="number">div </span>
+              <span class="then">></span>
+            </div>
+            <br />
+            <p class="fz28">場景: 一個類名 , 來回切換</p>
+            <br />
+            <div class="fz30">
+              <span class="then">＜</span> <span class="number">div </span>
+              <span class="function">class </span> <span class="then">=</span>
+              <span class="src">"box"</span>
+              <span class="function">:class</span>
+              <span class="then">=</span>
+              <span class="src">"[類名1,類名2,類名3]"</span>
+              <span class="then">>＜/ </span> <span class="number">div </span>
+              <span class="then">></span>
+            </div>
+            <br />
             <br />
             <br />
             <p>6. v-for</p>
@@ -340,85 +368,201 @@ function locate(idPound) {
             </div>
             <div class="padTwo VSL">
               <span class="then">＜</span> <span class="number">li</span>
-              <span class="word">v-for</span> <span class="then">=</span>
+              <span class="function">v-for</span> <span class="then">=</span>
               <span class="src">"(item,index) in list" </span>
-              <span class="word">:key</span> <span class="then">=</span>
+              <span class="function">:key</span> <span class="then">=</span>
               <span class="src">"item.id"</span> <span class="then">></span>
               <div></div>
               <div class="padTwo VSL">
                 <span class="then">＜</span> <span class="number">span</span>
-                <span class="word">>　｛｛item.name｝｝</span>
+                <span class="then">></span>
+                <span class="word">｛｛item.name｝｝</span>
                 <span class="then">＜/</span> <span class="number">span</span>
                 <span class="then">></span>
+                <div>
+                  <span class="then">＜</span>
+                  <span class="number">button </span>
+                  <span class="then">@click = </span>
+                  <span class="src">"del(item.id)"</span>
+                  <span class="then">></span> <span class="word">刪除</span>
+                  <span class="then">＜/</span>
+                  <span class="number">button </span>
+                  <span class="then">></span>
+                </div>
                 <div></div>
               </div>
+              <div>
+                <span class="then">＜/</span> <span class="number">li </span>
+                <span class="then">></span>
+              </div>
             </div>
-
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+            <div>
+              <span class="then">＜/</span> <span class="number">ul </span>
+              <span class="then">></span>
+            </div>
+            <br />
+            <br />
+            <p>key 作用: 給元素 添加的 唯一標識 , vue 能正確排序復用</p>
+            <br />
+            <p>有加 key 列表正確刪除 (ex第一項) , 下面三項往上</p>
+            <p>沒加 key , v-for 會嘗試 原地修改元素 (就地復用)</p>
+            <p>找前三個 , 把文字內容 替換到 前三個 li 身上 (ex背景顏色仍在)</p>
+            <br />
+            <br />
+            <br />
+            <p>7. v-model</p>
+            <p>
+              給 表單元素 使用 , 雙向數據綁定 -> 可以快速 獲取 或 設置
+              表單元素內容
+            </p>
+            <p>視圖變化 -> 數據自動更新</p>
+            <p>語法: v-model="變量"</p>
+            <br />
+            <br />
+            <p class="fz32 cor36">v-model 用於其他表單</p>
+            <br />
+            <p class="fz28">
+              常見表單 都可用 v-model -> 快速 獲取 & 設置 表單元素的 值
+            </p>
+            <br />
+            <p class="fz28">輸入框 input: text -> value</p>
+            <br />
+            <p class="fz28">文本域 textarea -> value</p>
+            <br />
+            <p class="fz28">複選框 input: checkbox -> checked</p>
+            <br />
+            <p class="fz28">單選框 input: radio -> checked</p>
+            <br />
+            <p class="fz28">下拉菜單 select -> value</p>
+            <br />
+            <br />
           </div>
-          <p class="fz28"></p>
+          <p class="fz28">指令修飾符</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">1.按鍵</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">@keyup.enter -> 鍵盤按 enter 監聽</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">2.v-model</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">v-model.trim -> 去除首尾空格</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">v-model.number -> 轉數字</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">3.事件</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">@事件名.stop -> 阻止冒泡</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">@事件名.prevent -> 阻止默認行為</p>
           <br />
-          <p class="fz28"></p>
           <br />
-          <p class="fz28"></p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz32 cor36">計算屬性 computed</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">
+            基於現有數據 , 計算出來的新屬性 , 依賴的數據變化 , 自動重新計算
+          </p>
           <br />
-          <p class="fz28"></p>
+
+          <p class="fz28">
+            語法: 聲明在 computed 中 , 一個計算屬性對應一個函數
+          </p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">使用 ｛｛ 計算屬性名 ｝｝</p>
           <br />
-          <p class="fz28"></p>
+          <div class="fz30">
+            <div>
+              <span class="word">computed:</span>
+              <span class="brackets">{</span>
+            </div>
+            <div class="padTwo VSL">
+              <div>
+                <span class="comment"
+                  >//計算屬性:有緩存的,一旦計算出來結果,就會立刻緩存</span
+                >
+              </div>
+              <div>
+                <span class="comment"
+                  >//下一次讀取->直接讀緩存就行了->性能高</span
+                >
+              </div>
+              <span class="word">計算屬性名</span>
+              <span class="brackets">(){</span>
+              <div></div>
+              <div class="padTwo VSL">
+                <span class="comment">//基於現有數據,編寫求值邏輯</span>
+                <div>
+                  <span class="then">return </span>
+                  <span class="word">結果</span>
+                </div>
+                <div></div>
+              </div>
+              <div><span class="brackets">}</span></div>
+            </div>
+            <div><span class="brackets">}</span></div>
+          </div>
           <br />
-          <p class="fz28"></p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">計算屬性完整寫法</p>
           <br />
-          <p class="fz28"></p>
+          <div class="fz30">
+            <div>
+              <span class="word">computed:</span>
+              <span class="brackets">{</span>
+            </div>
+            <div class="padTwo VSL">
+              <span class="word">計算屬性名</span>
+              <span class="brackets">{</span>
+              <div class="padTwo VSL">
+                <div>
+                  <span class="variable">get</span>
+                  <span class="brackets">(){</span>
+                </div>
+
+                <div class="padTwo VSL">
+                  <div>
+                    <span class="word">一段代碼邏輯</span>
+                    <span class="brackets">(</span>
+                    <span class="word">計算邏輯</span>
+                    <span class="brackets">)</span>
+                  </div>
+                  <div>
+                    <span class="then">return </span>
+                    <span class="word">結果</span>
+                  </div>
+                  <div></div>
+                </div>
+                <div><span class="brackets">},</span></div>
+                <div>
+                  <span class="variable">set</span>
+                  <span class="brackets">(</span>
+                  <span class="word">修改的值</span>
+                  <span class="brackets">){</span>
+                </div>
+                <div class="padTwo VSL">
+                  <span class="word">一段代碼邏輯</span>
+                  <span class="brackets">(</span>
+                  <span class="word">修改邏輯</span>
+                  <span class="brackets">)</span>
+                  <div></div>
+                </div>
+                <div><span class="brackets">}</span></div>
+              </div>
+
+              <div><span class="brackets">}</span></div>
+            </div>
+            <div><span class="brackets">}</span></div>
+          </div>
           <br />
-          <p class="fz28"></p>
           <br />
-          <p class="fz28"></p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz32 cor36">watch 偵聽器</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">監視數據變化 , 執行一些 業務邏輯 或 異步操作</p>
           <br />
-          <p class="fz28"></p>
+          <p class="fz28">語法:</p>
           <br />
-          <p class="fz28"></p>
-          <br />
-          <p class="fz28"></p>
-          <br />
-          <p class="fz28"></p>
-          <br />
-          <p class="fz28"></p>
-          <br />
-          <p class="fz28"></p>
+          <p class="fz28">簡單寫法 -> 簡單數據類型 , 直接監視</p>
           <br />
           <p class="fz28"></p>
           <br />
